197bdee0a6e22ce7825ace3e4bd1fff22b50ad47
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2022-11-14 22:29:40 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <nav className={"rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"}>
+            <h1 className={"text-3xl font-bold underline"}>My Public Diary</h1>
+        </nav>
+        <div>
+            <div className="md:container md:mx-auto">
+                <div className="mt-10 mb-5 flex flex-row space-x-4">
+                    <input className="" type="text" placeholder={"what did you do today?"}/>
+                    <button>Submit</button>
+                </div>
+                <div className="flex flex-col">
+                    <div className="rounded-lg p-4 box-border md:box-content w-50 md:w-100 lg:w-48 bg-slate-700 text-white hover:bg-slate-500">
+                        <h1 className="text-lg inline-block">
+                            Title
+                        </h1>
+                        <p className="text-amber-50">
+                            this is the content
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
   );
 }
 
