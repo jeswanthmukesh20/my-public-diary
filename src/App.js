4f0903db7b99b2429256461f402d0a584e2f9e7e
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2022-11-14 23:23:29 +0530

    changed layout

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@
 import './index.css';
 
 function App() {
-  return (
-    <div>
-        <nav className={"rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"}>
-            <h1 className={"text-3xl font-bold underline"}>My Public Diary</h1>
-        </nav>
+    return (
         <div>
-            <div className="md:container md:mx-auto">
-                <div className="mt-10 mb-5 flex flex-row space-x-4">
-                    <input className="" type="text" placeholder={"what did you do today?"}/>
-                    <button>Submit</button>
-                </div>
-                <div className="flex flex-col">
-                    <div className="rounded-lg p-4 box-border md:box-content w-50 md:w-100 lg:w-48 bg-slate-700 text-white hover:bg-slate-500">
-                        <h1 className="text-lg inline-block">
-                            Title
-                        </h1>
-                        <p className="text-amber-50">
-                            this is the content
-                        </p>
+            <nav className={"rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"}>
+                <h1 className={"text-3xl font-bold underline"}>My Public Diary</h1>
+            </nav>
+            <div>
+                <div className="md:container lg:w-50 md:mx-auto">
+                    <div className="mt-10 mb-5 flex flex-row space-x-4">
+                        <input className="" type="text" placeholder={"what did you do today?"}/>
+                        <button className="bg-black text-white p-2 rounded hover:bg-gray-700">Submit</button>
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="rounded-lg p-4 box-border md:box-content w-100 md:w-100 lg:w-50 bg-slate-700 text-white hover:bg-slate-500">
+                            <h1 className="text-lg inline-block">
+                                Title
+                            </h1>
+                            <p className="text-amber-50">
+                                Hello world
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-  );
+    );
 }
 
 export default App;
