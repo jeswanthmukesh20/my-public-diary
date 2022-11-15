0082a47812d40fb62e123c9250d0c45fb5742d2b
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2022-11-15 13:29:23 +0530

    integrated database and fixed ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
-
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
+import {useState, useEffect} from 'react';
 import './index.css';
 
+const Card = ({title, author, content, date}) => {
+    return (
+        <div className="rounded-lg p-4 box-border mt-5 w-4/5 sm:w-4/5 md:w-4/5 lg:w-4/5 bg-slate-100 text-black hover:bg-slate-50 shadow-md hover:drop-shadow-xl">
+            <h1 className="text-lg font-bold mb-5 block">
+                {title}
+            </h1>
+            <p className="text-gray-700">
+                {content}
+            </p>
+            <div className="flex  text-xs right-5 justify-end block text-gray-500">
+                <p className="ml-5">@{`${author}  ${date}`}</p>
+            </div>
+        </div>
+    )
+}
+
+
 function App() {
+    const [author, setAuthor] = useState("");
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [data, setData] = useState([]);
+    const current = new Date();
+    const currentOffset = current.getTimezoneOffset();
+    const date = new Date(current.getTime() + (330 + currentOffset)*60000);
+    const handleSubmit = () => {
+        if(author !== "" && title !== "" && content !== ""){
+            let dat = [];
+            dat.push(...data)
+            dat.push({
+                title: title,
+                content: content,
+                author: author
+            })
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title: title,  content: content, author: author})
+            };
+            fetch("https://3c8eql.deta.dev/store", requestOptions).then(resp => {
+                console.log(resp)
+            }).then(() => {
+                setContent("");
+                setTitle("");
+                setAuthor("");
+            })
+            setData(dat);
+        }
+    }
+    useEffect(() => {
+        fetch("https://3c8eql.deta.dev/get").then(res => res.json()).then(json => {
+            setData(json.resp);
+        })
+    }, [null])
     return (
         <div>
             <nav className={"rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"}>
-                <h1 className={"text-3xl font-bold underline"}>My Public Diary</h1>
+                <h1 className={"text-3xl font-bold"}>My Public Diary</h1>
             </nav>
             <div>
-                <div className="md:container lg:w-50 md:mx-auto">
-                    <div className="mt-10 mb-5 flex flex-row space-x-4">
-                        <input className="" type="text" placeholder={"what did you do today?"}/>
-                        <button className="bg-black text-white p-2 rounded hover:bg-gray-700">Submit</button>
+                <div className="sm:container lg:w-3/5 md:w-3/5 sm:w-3/5 mx-3 sm:mx-auto">
+                    <div className="mt-10 mb-5 flex flex-col  space-y-4  justify-items-center">
+                        <input className="border-solid border-2 py-2 px-3 rounded border-gray-600" onChange={(e) => {setAuthor(e.target.value);  console.log(author)}} value={author} type="text" placeholder={"author"}/>
+                        <input className="border-solid border-2 py-2 px-3 rounded border-gray-600" onChange={(e) => {setTitle(e.target.value)}} value={title} type="text" placeholder={"Title"}/>
+                        <textarea className="border-solid pt-5 border-2 py-4 px-3 rounded border-gray-600" onChange={(e) => {setContent(e.target.value)}} value={content} type="text" placeholder={"what did you do today?"}/>
+                        <button onClick={() => {
+                            handleSubmit()
+                            console.log(data)
+                        }} className="bg-black  place-self-center w-2/3 text-white p-2 rounded hover:bg-gray-700">Submit</button>
+                        
                     </div>
-                    <div className="flex flex-col">
-                        <div className="rounded-lg p-4 box-border md:box-content w-100 md:w-100 lg:w-50 bg-slate-700 text-white hover:bg-slate-500">
-                            <h1 className="text-lg inline-block">
-                                Title
-                            </h1>
-                            <p className="text-amber-50">
-                                Hello world
-                            </p>
-                        </div>
+                    <div className="flex mt-20 flex-col-reverse items-center">
+                        {data.map(blogs => {
+                            console.log(typeof blogs.date)
+                            let resp_date = new Date(blogs.date)
+                            resp_date = `${resp_date.toLocaleTimeString()} ${resp_date.toLocaleDateString()}`
+                            return <Card title={blogs.title} content={blogs.content} author={blogs.author} date={resp_date} />
+                        })}
                     </div>
+
+                   
                 </div>
             </div>
         </div>
